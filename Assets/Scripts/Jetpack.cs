56b403940c753dabc19adfaf9e5fb24017f816f8
davidrocha9 <up201806371@fe.up.pt>
2022-03-12 23:14:56 +0000

    Added propulsion animation

--- Assets/Scripts/Jetpack.cs
+++ Assets/Scripts/Jetpack.cs
@@ -6,19 +6,32 @@ public class Jetpack : MonoBehaviour
 {
     public int speed, jumpforce;
     Rigidbody2D rb;
+    public Sprite flying, running;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        animator.SetFloat("Height", transform.position.y);
+        if (animator)
         if (Input.GetKey("up"))
         {
             rb.AddForce(Vector2.up*jumpforce);
             //transform.position = new Vector2(0, Mathf.Clamp(transform.position.x, -2f, 2f));
+        }
+        if (transform.position.y == -5.512523)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = running;
+        }
+        else {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = flying;
         }
     }
 }
