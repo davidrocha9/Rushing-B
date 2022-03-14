30f8c8097cd3b3f90f8e5e56a2d06e3a480ced95
davidrocha9 <up201806371@fe.up.pt>
2022-03-14 03:30:56 +0000

    Added counter for meters

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -7,7 +7,8 @@ public class ScoreController : MonoBehaviour
 {
     public static ScoreController instance;
     public TextMeshProUGUI text;
-    int score = 0;
+    public TextMeshProUGUI meters;
+    int score = 0, currentMeters = 0, incrementMetersCounter = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,17 @@ public class ScoreController : MonoBehaviour
         if (instance == null)
         {
             instance = this;
+        }
+    }
+
+    void Update()
+    {
+        incrementMetersCounter += 1;
+        if (incrementMetersCounter > 10)
+        {
+            currentMeters += 1;
+            meters.text = currentMeters.ToString().PadLeft(4, '0');
+            incrementMetersCounter = 0;
         }
     }
 
