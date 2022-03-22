233efe107ea8b1239e29f9eebd917bf9b01267e7
Miguel Silva <up201806388@fe.up.pt>
2022-03-22 02:19:26 +0000

    shooting

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") || Input.GetKeyDown("space"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        //shooting logic
+        Instantiate(bulletPrefab, new Vector3((float)(firePoint.position.x + 0.4), firePoint.position.y, firePoint.position.z), firePoint.rotation);
+    }
+}
