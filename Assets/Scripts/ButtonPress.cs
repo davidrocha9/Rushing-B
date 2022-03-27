6b853eaae28f63080ebea84c42e6cfa0d2bd7126
davidrocha9 <up201806371@fe.up.pt>
2022-03-27 16:49:04 +0100

    Implemented Start Menu

---
+++ Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    public Sprite sprite1, sprite2;
+    SpriteRenderer spriteRenderer;
+    Animator animator, parentAnimator;
+    private Animation anim;
+    public GameObject game;
+    public GameObject spawner;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        animator = gameObject.GetComponent<Animator>();
+        parentAnimator = gameObject.transform.parent.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnMouseDown() 
+    {
+        animator.Play("PressedDown", -1, 0f);
+        parentAnimator.SetBool("Start", true);
+        game.SetActive(true);
+        spawner .SetActive(true);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,14 +8,13 @@ public class Player : MonoBehaviour
     public GameOverScreen GameOverScreen;
     public bool alive = true;
     Rigidbody2D rb;
-    public BoxCollider2D bc;
-    public Animator animator;
+    Animator animator;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        bc = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
     }
 
@@ -27,27 +26,30 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Coins"))
+        if (alive) 
         {
-            Destroy(other.gameObject);
-            ScoreController.instance.increaseCoins();
-        }
-        else if (other.gameObject.CompareTag("Notebook"))
-        {
-            Destroy(other.gameObject);
-            ScoreController.instance.increaseNotebooks();
-        }
-        else if (other.gameObject.CompareTag("TrashCan"))
-        {
-            alive = false;
-            GameOverScreen.Setup();
-            animator.SetBool("Dirty", true);
-        }
-        else if (other.gameObject.CompareTag("LightBulb"))
-        {
-            alive = false;
-            GameOverScreen.Setup();
-            animator.SetBool("Shocked", true);
+            if (other.gameObject.CompareTag("Coins"))
+            {
+                Destroy(other.gameObject);
+                ScoreController.instance.increaseCoins();
+            }
+            else if (other.gameObject.CompareTag("Notebook"))
+            {
+                Destroy(other.gameObject);
+                ScoreController.instance.increaseNotebooks();
+            }
+            else if (other.gameObject.CompareTag("TrashCan"))
+            {
+                alive = false;
+                GameOverScreen.Setup();
+                animator.SetBool("Dirty", true);
+            }
+            else if (other.gameObject.CompareTag("LightBulb"))
+            {
+                alive = false;
+                GameOverScreen.Setup();
+                animator.SetBool("Shocked", true);
+            }
         }
     }
 }
